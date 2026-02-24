6043da7b77a316bb9b4600b53868fdd78d2753c5
Symun-Hossain-Shifat <saymonshifat569625@gmail.com>
2026-02-24 14:29:07 -0800

    add js commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+// add all element
+let totalJob = document.getElementById("total");
+let InterviewJob =document.getElementById('interview');
+let rejectedJob = document.getElementById('rejected');
+let maincontainer = document.getElementById('cardscontainer');
+const setText = document.getElementById("noText");
+const setText2 = document.getElementById('totalJobfield');
+// catch the Main button
+const allbtn = document.getElementById('button-all');
+const interviewbtn = document.getElementById('button-interview');
+const rejectedbtn = document.getElementById('button-rejected');
+const mainsection = document.querySelector('main');
+console.log(mainsection);
+
+let interviewList = [];
+let rejectedList = [];
+
